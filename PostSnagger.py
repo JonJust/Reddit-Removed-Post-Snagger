e4f65780421fe28561bae49f5911b188b96aa342
Jonathan Just <53791986+JonJust@users.noreply.github.com>
2020-02-08 14:59:36 -0500

    Update PostSnagger.py

--- PostSnagger.py
+++ PostSnagger.py
@@ -17,7 +17,7 @@ def main():
     # Make <username> a mod on subreddit to be crawled. This will allow the bot access to the mod log.
     # Make sure to have your reddit app registered, plug your client id and secret client into this script.
     # Also, make sure to pip install praw before running. To learn more about praw visit https://praw.readthedocs.io/en/latest/
-    # I would also reccomend modding the bot, and adding it as an approved submitter on the target subreddit; else spam filters will happen.
+    # I would also recomend making the bot a mod and an approved submitter on the target subreddit; else spam filters will happen.
 
     # Should spam filters continue to be an issue, set up automoderator on the target subreddit.
     # In the config, add this (Make sure to remove the leftmost pound signs):
